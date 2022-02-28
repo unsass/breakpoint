e005c32a300ecf8185335dc03d199bd4265a5b50
Boris Damevin <borisdamevin@gmail.com>
2022-02-28 21:57:27 +0100

    feat(up, down, only, between): add main mixins
    transfer form `@sass-collective/breakpoint`

---
+++ tests/scss.spec.js
@@ -0,0 +1,44 @@
+// ============================================================================================= //
+//                                             TESTS                                             //
+// ============================================================================================= //
+
+const path = require('path');
+const sassTrue = require('sass-true');
+const glob = require('glob');
+
+/**
+ *
+ * @param url
+ * @param prev
+ * @param done
+ * @returns {{file: string}}
+ */
+function importer(url, prev, done) {
+    if (url[0] === '~') {
+        url = path.resolve('node_modules', url.substr(1));
+    }
+
+    return {
+        file: url
+    };
+}
+
+describe('Sass', () => {
+    // Find all the Sass files that end in `*.spec.scss` in any directory of this project.
+    // I use path.resolve because True requires absolute paths to compile test files.
+    const sassTestFiles = glob.sync(path.resolve(process.cwd(), '**/*.spec.scss'));
+
+    // Run True on every file found with to describe and its methods provided
+    sassTestFiles.forEach((file) =>
+        sassTrue.runSass({
+            importer,
+            file,
+            includePaths: [
+                './node_modules'
+            ]
+        }, {
+            describe,
+            it
+        })
+    );
+});
