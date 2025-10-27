0504a4b76822489718bd5afdbe88264a80adb68f
Boris Damevin <borisdamevin@gmail.com>
2025-10-27 21:40:50 +0100

    chore(deps): bump to `vitest` 4.0.4

--- vitest.config.ts
+++ vitest.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
             ],
             [
                 'junit', {
+                    suiteName: 'Tests',
                     outputFile: './junit.xml'
                 }
             ]
